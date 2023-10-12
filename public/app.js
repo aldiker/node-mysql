e7697aa4cd5d83853acd172d8f5ca577d449a548
aldiker <aldiker@ukr.net>
2023-10-12 10:57:35 +0300

    Added output array of orders to HTML

--- public/app.js
+++ public/app.js
@@ -1,35 +1,85 @@
-import { date, capitalize } from './filters.js'
+import { date, pluralizeTasks, capitalize } from './filters.js'
 import { addTodo } from './methods.js'
 
-function pluralizeTasks(count) {
-    if (count === 1) {
-        return 'задача'
-    } else if (count >= 2 && count <= 4) {
-        return 'задачи'
-    } else {
-        return 'задач'
-    }
-}
-
 function updateTaskList(todos) {
-    const cardCountElement = document.getElementById('card-count')
+    // Длина массива задач
     const todosLenght = state.todos.length
+
+    const cardCountElement = document.getElementById('card-count')
     cardCountElement.textContent = `${todosLenght} ${pluralizeTasks(
         todosLenght
     )}`
 
     const ordersTitleElement = document.getElementById('orders-title')
-    ordersTitleElement.textContent = 'Ваши задачи:'
+    if (todosLenght) {
+        ordersTitleElement.textContent = 'Ваши задачи:'
+    } else {
+        ordersTitleElement.textContent = 'У Вас 0 задач, добавьте новую'
+    }
 
     const taskListElement = document.getElementById('task-list')
     // Очищаем предыдущий список задач
     taskListElement.innerHTML = ''
 
     // Перебираем задачи в объекте state.todos и добавляем их на страницу
-    todos.forEach((todo) => {
+    todos.forEach((todo, index) => {
         const taskElement = document.createElement('div')
-        taskElement.textContent = todo.title
+        // Генерируем уникальный ID для каждого чекбокса
+        const checkboxId = `checkbox-${index}`
+        const buttonDeleteId = `btn-delete-${index}`
+
+        taskElement.innerHTML = `
+            <p>ID: ${todo.id}</p>
+            <p>
+                <label>
+                    <input
+                        type="checkbox"
+                        class="filled-in"
+                        ${todo.done ? 'checked="checked"' : ''}
+                        id="${checkboxId}"
+                    />
+                    <span>${todo.done ? 'Завершено' : 'Не завершено'}</span>
+                </label>
+            </p>
+            <p class=${todo.done ? 'order-done' : ''}>${todo.title}</p>
+            <button 
+                class="btn waves-effect waves-light" 
+                name="delete" 
+                data-todo-id="${todo.id}"
+                id="${buttonDeleteId}"
+            >
+                Удалить
+            </button>
+            <p>${todo.date}</p>
+            <br>
+        `
         taskListElement.appendChild(taskElement)
+
+        // Добавляем слушатель событий для чекбокса
+        const checkbox = document.getElementById(checkboxId)
+        checkbox.addEventListener('change', (event) => {
+            // Обновляем значение todo.done в соответствии с состоянием чекбокса
+            todo.done = event.target.checked
+            // Вызываем функцию обновления списка задач после изменения
+            updateTaskList(state.todos)
+        })
+
+        // Добавляем слушатель событий для кнопки удалить
+        const deleteButton = document.getElementById(buttonDeleteId)
+        deleteButton.addEventListener('click', function () {
+            // Берем индекс задачи, которую надо удалить
+            const todoId = this.getAttribute('data-todo-id')
+            // Находим индекс задачи в массиве по todoId и удаляем ее
+            const todoIndex = state.todos.findIndex(
+                (t) => t.id.toString() === todoId.toString()
+            )
+            // Если нашли, то удаляем один элемент начиная с найденного индекса
+            if (todoIndex !== -1) {
+                state.todos.splice(todoIndex, 1)
+            }
+            // После удаления задачи, обновляем отображение списка задач
+            updateTaskList(state.todos)
+        })
     })
 }
 
--- public/filters.js
+++ public/filters.js
@@ -9,3 +9,13 @@ export function date() {
         day: '2-digit',
     }).format(new Date())
 }
+
+export function pluralizeTasks(count) {
+    if (count === 1) {
+        return 'задача'
+    } else if (count >= 2 && count <= 4) {
+        return 'задачи'
+    } else {
+        return 'задач'
+    }
+}
